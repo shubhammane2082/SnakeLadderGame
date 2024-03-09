76c0d14668d68df55e8bfc71b697c4612c12e1cd
shubhammane2082 <shubhammane2082@gmail.com>
2024-03-09 19:58:16 +0530

    Set Initial Position of Player

---
+++ SnakeLadderGame/src/com/java/snakeladder/SnakeLadderMain.java
@@ -0,0 +1,11 @@
+package com.java.snakeladder;
+
+public class SnakeLadderMain 
+{
+	static int initialPositionplayer1=0;
+	public static void main(String[] args) 
+	{
+		System.out.println(initialPositionplayer1);
+	}
+
+}
---
+++ SnakeLadderGame/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author shubh
+ *
+ */
+module SnakeLadderGame {
+}
