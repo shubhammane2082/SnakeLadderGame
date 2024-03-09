2e6884a966df292b0fe6bf5d81c339e0f9e43c7a
shubhammane2082 <shubhammane2082@gmail.com>
2024-03-09 20:15:13 +0530

    Apply Condition to Specify Winning Number

--- SnakeLadderGame/src/com/java/snakeladder/SnakeLadderMain.java
+++ SnakeLadderGame/src/com/java/snakeladder/SnakeLadderMain.java
@@ -5,27 +5,31 @@ public class SnakeLadderMain
 	static int initialPositionplayer1=0,currentPositionPlayer1=0;
 	static int diceNumber1=0;
 	static int option1=0;
+	static final int goal=100;
 	public static void main(String[] args) 
 	{
-		diceNumber1=PlayerDice1.diceNumber1();
-		
-		option1=Checkoption1.checkoption1();
-		
-		switch(option1)
+		while(currentPositionPlayer1 < goal)
 		{
-		   case 0 :
-			 System.out.println("No Play...");
-			 break;
-		   case 1 :
-			 currentPositionPlayer1 += diceNumber1;
-			 break;
-		   case 2: 
-			currentPositionPlayer1 -= diceNumber1;
-			break;
+			diceNumber1=PlayerDice1.diceNumber1();
+			
+			option1=Checkoption1.checkoption1();
+			
+			switch(option1)
+			{
+			   case 0 :
+				 System.out.println("No Play...");
+				 break;
+			   case 1 :
+				 currentPositionPlayer1 += diceNumber1;
+				 break;
+			   case 2: 
+				currentPositionPlayer1 -= diceNumber1;
+				break;
+			}
+			
+			
+			System.out.println("Current Position of player is : "+currentPositionPlayer1);
 		}
-		
-		System.out.println("Current Position of player is : "+currentPositionPlayer1);
-		
 	}
 
 }
