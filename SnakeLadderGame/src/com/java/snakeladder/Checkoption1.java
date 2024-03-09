e9537a93ba9dfe3e0b0e7c3113ae6dabccf58be9
shubhammane2082 <shubhammane2082@gmail.com>
2024-03-09 20:08:32 +0530

    Check Option to Determine Current Position of Player

---
+++ SnakeLadderGame/src/com/java/snakeladder/Checkoption1.java
@@ -0,0 +1,13 @@
+package com.java.snakeladder;
+
+import java.util.Random;
+
+public class Checkoption1 
+{
+	public static int checkoption1()
+	{
+		Random random=new Random();
+		return random.nextInt(3);
+	}
+
+}
--- SnakeLadderGame/src/com/java/snakeladder/SnakeLadderMain.java
+++ SnakeLadderGame/src/com/java/snakeladder/SnakeLadderMain.java
@@ -2,12 +2,29 @@ package com.java.snakeladder;
 
 public class SnakeLadderMain 
 {
-	static int initialPositionplayer1=0;
+	static int initialPositionplayer1=0,currentPositionPlayer1=0;
 	static int diceNumber1=0;
+	static int option1=0;
 	public static void main(String[] args) 
 	{
 		diceNumber1=PlayerDice1.diceNumber1();
-		System.out.println(diceNumber1);
+		
+		option1=Checkoption1.checkoption1();
+		
+		switch(option1)
+		{
+		   case 0 :
+			 System.out.println("No Play...");
+			 break;
+		   case 1 :
+			 currentPositionPlayer1 += diceNumber1;
+			 break;
+		   case 2: 
+			currentPositionPlayer1 -= diceNumber1;
+			break;
+		}
+		
+		System.out.println("Current Position of player is : "+currentPositionPlayer1);
 		
 	}
 
